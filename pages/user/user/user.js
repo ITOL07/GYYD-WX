265f80ca3c27c701c3c1d1dbd1c55f60eaf34039
ITOL <1104026790@qq.com>
2019-03-31 19:42:26 +0800

    增加获取微信头像

--- pages/user/user/user.js
+++ pages/user/user/user.js
@@ -9,7 +9,10 @@ Page({
    */
   data: {
     actionSheetHidden: true,
-    headImage: '../../../static/images/user/head.png'
+    headImage: '../../../static/images/user/head.png',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   headImageClick: function () {
     this.setData({
@@ -77,11 +80,45 @@ Page({
     var setTitle = '设置';
     commonData.routers(setRouter, setTitle);
   },
+  getUserInfo: function (e) {
+    // console.log(e)
+    console.log(e.detail.userInfo)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
