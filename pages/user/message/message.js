5a32be77e28010af349ed2ae716a744773353bda
jojo <jojo@163.com>
2019-05-10 10:11:55 +0800

    优化消息页面
    增加消息的筛选，点击课程提醒与系统通知将会在页面自动筛选数据

--- pages/user/message/message.js
+++ pages/user/message/message.js
@@ -1,7 +1,7 @@
 // pages/message/message.js
 const app = getApp()
 var fileData = require("../../../utils/data.js");
-
+var map = []
 Page({
 
   /**
@@ -16,19 +16,42 @@ Page({
 		this.setData({
 			selected: true
 		})
+		var data = []
+		if(this.data.selected==true){
+			for(var index in map){
+				if(map[index].type=="课程提醒"){
+					data.push(map[index])
+				}
+			}
+			this.setData({
+				messageData: data
+			})
+		}
 	},
 
 	switch2: function () {
 		this.setData({
 			selected: false
 		})
+		var data = []
+		if (this.data.selected == false) {
+			for (var index in map) {
+				if (map[index].type == "系统通知") {
+					data.push(map[index])
+				}
+			}
+			this.setData({
+				messageData: data
+			})
+		}
 	},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+		map = this.data.messageData
+		this.switch1()
   },
 
   /**
