319048917d406a36a9dece6ed24faa2f261b3ada
ITOL <1104026790@qq.com>
2019-06-05 14:43:28 +0800

    20190605

--- pages/user/reg/reg.js
+++ pages/user/reg/reg.js
@@ -187,7 +187,8 @@ Page({
         method: "POST",
         data: {
           phoneNo: phone,
-          passwd: util.hexMD5(that.data.NewChanges)
+          passwd: util.hexMD5(that.data.NewChanges),
+          type:1
         },
         header: {
           "content-type": "application/x-www-form-urlencoded"
