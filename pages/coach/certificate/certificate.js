6efbff63aaa821ae061c7624cc0751927b771cbb
sun <sun@163.com>
2019-06-10 23:44:35 +0800

    图片预加载

--- pages/coach/certificate/certificate.js
+++ pages/coach/certificate/certificate.js
@@ -1,82 +1,109 @@
-// pages/certificate/certificate.js
-
 const app = getApp()
 var fileData = require("../../../utils/data.js");
 var commonData = require("../../../utils/util.js");
+const ImgLoader = require("../../../utils/img-loader.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    icons_url:[],
+		icons_url: [],
     icons_name:[],
     coach_id: app.globalData.user_id,
     type:'23',
-    public_url:app.globalData.public_url,
-    index:0
+    public_url:app.globalData.public_url
   },
+
+	genImgListData: function (options) {
+		var images = []
+		var that = this
+		var url_tmp = fileData.getListConfig().url_test;
+		wx.request({
+			url: url_tmp + '/userIcons/queryCoachInfoIcons',
+			method: 'POST',
+			data: {
+				coach_id: options.coachid,
+				type: options.type
+			},//param
+			header: {
+				'content-type': 'application/x-www-form-urlencoded'  //发送post请求
+			},
+			success: function (res) {
+				if (res.statusCode == 200) {
+					images = res.data.icons_url
+					var img = images.map(item => {
+							return {
+								url: item,
+								loaded: false
+							}
+					})
+					console.log("images:"+JSON.stringify(img)+"---end")
+					that.setData({
+						icons_url: img
+					})
+				}
+			}
+		})
+		
+	},
+
+	loadImages: function() {
+		//同时发起全部图片的加载
+		this.data.icons_url.forEach(item => {
+			this.imgLoader.load(item.url)
+		})
+	},
+	//加载完成后的回调
+	imageOnLoad(err, data) {
+		console.log('图片加载完成', err, data.src)
+
+		const icons_url = this.data.icons_url.map(item => {
+			if (item.url == data.src)
+				item.loaded = true
+			return item
+		})
+		this.setData({ icons_url })
+	},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
-    var that = this
-    var url_tmp = fileData.getListConfig().url_test;
-    wx.request({
-      url: url_tmp + '/userIcons/queryCoachInfoIcons',
-      method: 'POST',
-      data: { 
-        coach_id:options.coachid,
-        type: options.type
-        },//param
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'  //发送post请求
-      }, success: function (res) {
-        console.info(res)
-        if (res.statusCode == 200) {
-          that.setData({
-            icons_name: res.data.icons_name,
-            icons_url: res.data.icons_url
-          })
-        }
-        console.log(that.data.icons_name)
-        console.log(that.data.icons_url)
-      }
-    })
+		this.genImgListData(options)		
+		//初始化图片预加载组件，并指定统一的加载完成回调
+		this.imgLoader = new ImgLoader(this, this.imageOnLoad.bind(this))
+    // console.log(options)
+    // var that = this
+    // var url_tmp = fileData.getListConfig().url_test;
+    // wx.request({
+    //   url: url_tmp + '/userIcons/queryCoachInfoIcons',
+    //   method: 'POST',
+    //   data: { 
+    //     coach_id:options.coachid,
+    //     type: options.type
+    //   },
+    //   header: {
+    //     'content-type': 'application/x-www-form-urlencoded'  //发送post请求
+    //   }, 
+		// 	success: function (res) {
+    //     console.info(res)
+    //     if (res.statusCode == 200) {
+    //       that.setData({
+    //         icons_name: res.data.icons_name,
+    //         // icons_url: res.data.icons_url
+    //       })
+    //     }
+    //     console.log(that.data.icons_name)
+    //     // console.log(that.data.icons_url)
+    //   }
+    // })
   },
 
 	previewImg: function (e) {
 		commonData.previewImg(e, this.data.icons_url)
 	},
 
-  // changeImage:function(event){
-  //   var that = this
-  //   var num = that.data.index
-  //   var fx = event.currentTarget.dataset.param
-  //   var length = that.data.icons_url.length-1
-  //   console.log(length)
-  //   console.info(fx)
-  //   if(fx == 'left'){
-  //     if (num == 0) {
-  //       num = length
-  //     } else{
-  //       num = num-1
-  //     }
-  //   } else if (fx == 'right'){
-  //     if (num == length) {
-  //       num = 0
-  //     } else {
-  //       num = num + 1
-  //     }
-  //   }
-  //   console.log(num)
-  //     that.setData({
-  //       index:num
-  //     })
-    
-  // },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -88,7 +115,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+		var that = this
+		setTimeout(function(){
+			that.loadImages()
+		}, 1000)
   },
 
   /**
